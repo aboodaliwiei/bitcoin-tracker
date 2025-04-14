ee5f57068918f995c0455e9324d0167fb73f1b99
aboodaliwiei <arbesorelattakia@gmail.com>
2025-04-14 15:38:18 +0300

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -6,8 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'dist',
-    rollupOptions: {
-      input: './src/index.jsx',
-    },
+    emptyOutDir: true,
   },
+  server: {
+    host: true,
+    port: 5173
+  }
 })
